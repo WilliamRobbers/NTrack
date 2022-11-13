08c625a74040b2a7c997b26296c69371ba898371
WilliamRobbers <69828637+WilliamRobbers@users.noreply.github.com>
2022-11-14 10:16:13 +1300

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-for i, std in enumerate(std_list):  # Change original grade
-        #     if std[0] == entered_std_num:
-        #         std[1] = entered_original_grade
-        #         std[2] = entered_resit_grade
