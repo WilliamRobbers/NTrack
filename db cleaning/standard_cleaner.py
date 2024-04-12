dcdb9cad1a2679e426b28d31ae81c4265cff38f3
WilliamRobbers <69828637+WilliamRobbers@users.noreply.github.com>
2024-04-12 14:50:53 +1200

    update standard cleaner to include level 4 standards
    standard cleaner has been updated to include level 4 standard.
    
    master list has been updated to include level 4 standards.

--- db cleaning/standard_cleaner.py
+++ db cleaning/standard_cleaner.py
@@ -4,5 +4,5 @@ dirty_list = open("list-of-all-standards-2020.csv")
 clean_list = open("master-list.csv", "w", newline="")
 
 for line in csv.reader(dirty_list):
-    if line[7].lower() == "current" and line[4].lower() in ["1", "2", "3"] and line[6].lower() != "expired":
+    if line[7].lower() == "current" and line[4].lower() in ["1", "2", "3", "4"] and line[6].lower() != "expired":
         csv.writer(clean_list).writerow(line)
